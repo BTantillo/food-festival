7bb13b07a9e445f7fc53d382a54b2693875ad4e5
Mountaineer Ben <bentantillo@gmail.com>
2021-12-08 13:38:08 -0800

    added webpack loader

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 require("bootstrap");
+const img1 = require("../../assets/img/food-table.jpg");
+const img2 = require("../../assets/img/grill.jpg");
+
 
 $(document).ready(function() {
   // First image is hard coded in index.html
@@ -6,14 +9,14 @@ $(document).ready(function() {
     {
       title: "We travel all over the US",
       subtitle: "Check out our schedule!",
-      img: "./assets/img/food-table.jpg",
+      img: img1,
       btnText: "View Schedule",
       btnUrl: "schedule.html"
     },
     {
       title: "Our food is seriously the bomb!",
       subtitle: "What are you waiting for?",
-      img: "./assets/img/grill.jpg",
+      img: img2,
       btnText: "Purchase Tickets",
       btnUrl: "tickets.html"
     },
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,30 @@ module.exports = {
         filename: '[name].bundle.js',
         path: __dirname + '/dist'
     },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name (file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function(url) {
+                                return url.replace('../', '/assets/')
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
+    },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
