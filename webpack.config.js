87b63737416d82c4e0b61752d12133500466ba5b
Mountaineer Ben <bentantillo@gmail.com>
2021-12-08 12:44:37 -0800

    script.js split up, DOM method added

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 
 module.exports = {
@@ -12,6 +13,9 @@ module.exports = {
         new webpack.ProvidePlugin({
             $: "jquery",
             jquery: "jquery"
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", //the report outputs to an HTML file in the dist folder
         })
     ],
     mode: 'development'
